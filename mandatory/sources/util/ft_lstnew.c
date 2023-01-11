091518ee844161cb755a796f83e3604905d1ad0e
junyojeo <jjy8479@gmail.com>
2023-01-11 18:36:39 +0900

    1

--- mandatory/sources/util/ft_lstnew.c
+++ mandatory/sources/util/ft_lstnew.c
@@ -6,13 +6,13 @@
 /*   By: junyojeo <junyojeo@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 18:13:27 by junyojeo          #+#    #+#             */
-/*   Updated: 2023/01/09 09:20:46 by junyojeo         ###   ########.fr       */
+/*   Updated: 2023/01/09 10:46:00 by junyojeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_info	*ft_lstnew(t_stack *s, int val)
+t_info	*ft_lstnew(int val)
 {
 	t_info	*elem;
 	
@@ -20,7 +20,6 @@ t_info	*ft_lstnew(t_stack *s, int val)
 	if (!elem)
 		print_error("Memory allocation fail.\n");
 	elem->val = val;
-	elem->index = s->size;
 	elem->rank = 0;
 	return (elem);
 }
