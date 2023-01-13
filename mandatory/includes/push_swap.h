edde61f3f9b9a892af7e6dab2c012a176f45ddf8
junyojeo <jjy8479@gmail.com>
2023-01-14 08:25:41 +0900

    1

--- mandatory/includes/push_swap.h
+++ mandatory/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: junyojeo <junyojeo@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 21:08:07 by junyojeo          #+#    #+#             */
-/*   Updated: 2023/01/14 05:42:54 by junyojeo         ###   ########.fr       */
+/*   Updated: 2023/01/14 08:17:24 by junyojeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,8 +85,8 @@ void	stack_init(t_stack *a, t_stack *b);
 int		*parsing_stack_and_array(t_stack *a,  int argc, char **argv);
 void	check_duplicate_and_sort(int *arr, int size);
 void	ranked(t_stack *a, int *arr);
-void	a_to_b(t_stack *a, t_stack *b, int size, t_cnt *cnt);
-void	b_to_a(t_stack *a, t_stack *b, int size, t_cnt *cnt);
+void	a_to_b(t_stack *a, t_stack *b, int size);
+void	b_to_a(t_stack *a, t_stack *b, int size);
 void	quick_sort(t_stack *a, t_stack *b, int size);
 void	hard_coding(t_stack *a, t_stack *b, int size, char s);
 int		main(int ac, char **ar);
--- mandatory/sources/core/quick_sort.c
+++ mandatory/sources/core/quick_sort.c
@@ -6,7 +6,7 @@
 /*   By: junyojeo <junyojeo@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 18:24:40 by junyojeo          #+#    #+#             */
-/*   Updated: 2023/01/14 07:19:23 by junyojeo         ###   ########.fr       */
+/*   Updated: 2023/01/14 08:24:56 by junyojeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,15 +38,17 @@ static int	is_sorted(t_stack *s, int size)
 	return (1);
 }
 
-void	a_to_b(t_stack *a, t_stack *b, int size, t_cnt *cnt)
+void	a_to_b(t_stack *a, t_stack *b, int size)
 {
-	int	i;
-	int	pivot_min;
-	int	pivot_max;
+	t_cnt	cnt;
+	int		i;
+	int		pivot_min;
+	int		pivot_max;
 
+	cnt_init(&cnt);
 	pivot_min = size / 3;
 	pivot_max = size / 3 * 2;
-	if (size < 3)
+	if (size <= 3)
 	{
 		if (!is_sorted(a, size))
 			hard_coding(a, b, size, 'a');
@@ -58,80 +60,105 @@ void	a_to_b(t_stack *a, t_stack *b, int size, t_cnt *cnt)
 		if (a->top->rank >= pivot_max)
 		{
 			cmd(a, b, "r", 'a');
-			cnt->ra++;
+			cnt.ra++;
 		}
 		else
 		{
 			cmd(a, b, "p", 'b');
-			cnt->pb++;
-			if (a->top->rank >= pivot_min)
+			cnt.pb++;
+			if (b->top->rank >= pivot_min)
 			{
 				cmd(a, b, "r", 'b');
-				cnt->rb++;
+				cnt.rb++;
 			}
 		}
 	}
 	i = 0;
-	while (i < cnt->ra && i < cnt->rb)
+	while (i < cnt.ra)
 	{
-		cmd(a, b, "rrr", 'c');
+		cmd(a, b, "rr", 'a');
 		i++;
 	}
-	a_to_b(a, b, cnt->ra, cnt);
-	b_to_a(a, b, cnt->rb, cnt);
-	b_to_a(a, b, cnt->pb - cnt->rb, cnt);
-	printf("OK\n");
+	i = 0;
+	while (i < cnt.rb)
+	{
+		cmd(a, b, "rr", 'b');
+		i++;
+	}
+	a_to_b(a, b, cnt.ra);
+	b_to_a(a, b, cnt.rb);
+	b_to_a(a, b, cnt.pb - cnt.rb);
 }
 
-void	b_to_a(t_stack *a, t_stack *b, int size, t_cnt *cnt)
+void	b_to_a(t_stack *a, t_stack *b, int size)
 {
-	int	i;
-	int	pivot_min;
-	int	pivot_max;
+	t_cnt	cnt;
+	int		i;
+	int		pivot_min;
+	int		pivot_max;
 
+	cnt_init(&cnt);
 	pivot_min = size / 3;
 	pivot_max = size / 3 * 2;
 	if (size < 3)
 	{
 		if (!is_sorted(b, size))
 			hard_coding(a, b, size, 'b');
+			//b면 뒤집어서 정렬하기
 		return ;
 	}
 	i = -1;
 	while (++i < size)
 	{
-		if (a->top->rank < pivot_min)
+		if (b->top->rank < pivot_min)
 		{
 			cmd(a, b, "r", 'b');
-			cnt->rb++;
+			cnt.rb++;
 		}
 		else
 		{
 			cmd(a, b, "p", 'a');
-			cnt->pa++;
+			cnt.pa++;
 			if (a->top->rank < pivot_max)
 			{
 				cmd(a, b, "r", 'a');
-				cnt->ra++;
+				cnt.ra++;
 			}
 		}
 	}
-	a_to_b(a, b, cnt->pa - cnt->ra, cnt);
+	a_to_b(a, b, cnt.pa - cnt.ra);
 	i = 0;
-	while (i < cnt->ra && i < cnt->rb)
+	while (i < cnt.ra)
 	{
-		cmd(a, b, "rrr", 'c');
+		cmd(a, b, "rr", 'a');
 		i++;
 	}
-	a_to_b(a, b, cnt->ra, cnt);
-	b_to_a(a, b, cnt->rb, cnt);
+	i = 0;
+	while (i < cnt.rb)
+	{
+		cmd(a, b, "rr", 'b');
+		i++;
+	}
+	a_to_b(a, b, cnt.ra);
+	b_to_a(a, b, cnt.rb);
 }
 
 void	quick_sort(t_stack *a, t_stack *b, int size)
 {
-	t_cnt	cnt;
-
-	cnt_init(&cnt);
-	a_to_b(a, b, size, &cnt);
+	a_to_b(a, b, size);
+	t_info	*cur;
+	t_info	*cur2;
+	cur = a->top;
+	cur2 = b->top;
+	while (cur)
+	{
+		printf("1. val: %d, rank: %d\n", cur->val, cur->rank);
+		cur = cur->prev;
+	}
+	while (cur2)
+	{
+		printf("2. val: %d, rank: %d\n", cur2->val, cur2->rank);
+		cur2 = cur2->prev;
+	}
 }
 
