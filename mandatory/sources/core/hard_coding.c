6bd196511646077f30614360845d5c0720d89f53
junyojeo <jjy8479@gmail.com>
2023-02-03 18:22:05 +0900

    1

--- mandatory/sources/core/hard_coding.c
+++ mandatory/sources/core/hard_coding.c
@@ -108,7 +108,9 @@ static void	in_A(t_stack *s)
 
 void	hard_coding(t_stack *s, t_stack *s2, int size, char c)
 {
+	int	tmp;
 	int cnt;
+	int	pa_cnt;
 
 	if (size == 1)
 		return;
@@ -142,83 +144,99 @@ void	hard_coding(t_stack *s, t_stack *s2, int size, char c)
 		if (c == 'a')
 		{
 			pb(s, s2);
-			if (s->size <= 3)
-				in_A(s);
-			else
-				in_A_sort_three(s);
-			while (size--)
+			tmp = s2->top->val;
+			hard_coding(s, s2, 3, 'a');
+			if (tmp < first)
+				pa(s, s2);
+			else if (first < tmp && tmp < second)
 			{
-				if (first > s2->top->rank)
-					pa(s, s2);
-				else
-				{
-					ra(s);
-					cnt++;
-				}
+				pa(s, s2);
+				sa(s);
 			}
-			while (cnt--)
+			else if (second < tmp && tmp < third)
+			{
+				ra(s);
+				ra(s);
+				pa(s, s2);
 				rra(s);
+				rra(s);
+			}
+			else
+			{
+				pa(s, s2);
+				ra(s);
+			}
 		}
-		else if (c == 'b')
+		else if (c == 'a')
 		{
 			pa(s2, s);
-			if (s->size <= 3)
-				in_B(s);
-			else
-				in_B_sort_three(s);
-			while (size--)
+			tmp = s2->top->val;
+			hard_coding(s2, s, 3, 'a');
+			if (tmp > first)
+				pb(s, s2);
+			else if (first > tmp && tmp > second)
 			{
-				if (first < s2->top->rank)
-					pb(s2, s);
-				else
-				{
-					rb(s);
-					cnt++;
-				}
+				pb(s, s2);
+				sb(s);
 			}
-			while (cnt--)
+			else if (second > tmp && tmp > third)
+			{
+				rb(s);
+				rb(s);
+				pb(s, s2);
 				rrb(s);
+				rrb(s);
+			}
+			else
+			{
+				pa(s, s2);
+				ra(s);
+			}
 		}
+		//else if (c == 'b')
+		//{
+		//	pa(s2, s);
+		//	hard_coding(s2, s, 3, 'b');
+		//	tmp = s2->top->rank;
+		//	if (tmp > first)
+		//		pb(s, s2);
+		//	else if (first > tmp && tmp > second)
+		//	{
+		//		pb(s, s2);
+		//		sb(s);
+		//	}
+		//	else if (second > tmp && tmp > third)
+		//	{
+		//		rb(s);
+		//		rb(s);
+		//		pb(s, s2);
+		//		rrb(s);
+		//		rrb(s);
+		//	}
+		//	else
+		//	{
+		//		pb(s, s2);
+		//		rb(s);
+		//	}
+		//}
 	}
 	else if (size == 5)
 	{
+		pa_cnt = 0;
 		cnt = 0;
 		if (c == 'a')
 		{
 			pb(s, s2);
 			pb(s, s2);
-			hard_coding(s, s2, 2, 'a');
-			while (size--)
-			{
-				if (first > s2->top->rank)
-					pa(s, s2);
-				else
-				{
-					ra(s);
-					cnt++;
-				}
-			}
-			while (cnt--)
-				rra(s);
+			hard_coding(s, s2, 3, 'a');
+			
 		}
 		else if (c == 'b')
 		{
 			pa(s2, s);
 			pa(s2, s);
-			hard_coding(s2, s, 2, 'b');
-			while (size--)
-			{
-				if (first < s2->top->rank)
-					pb(s2, s);
-				else
-				{
-					rb(s);
-					cnt++;
-				}
-			}
-			while (cnt--)
-				rrb(s);
-		
+			hard_coding(s2, s, 3, 'b');
+			
 		}
 	}
 }
